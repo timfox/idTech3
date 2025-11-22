53601c750f066c564ed678eefe8d25e60d600ba5
timfox <timfox@outlook.com>
2025-11-21 23:42:02 -0800

    Add "exit" command as an alias for "quit" in Com_Init function

--- code/qcommon/common.c
+++ code/qcommon/common.c
@@ -3945,6 +3945,7 @@ void Com_Init( char *commandLine ) {
 	}
 
 	Cmd_AddCommand( "quit", Com_Quit_f );
+	Cmd_AddCommand( "exit", Com_Quit_f ); // Alias for quit
 	Cmd_AddCommand( "changeVectors", MSG_ReportChangeVectors_f );
 	Cmd_AddCommand( "writeconfig", Com_WriteConfig_f );
 	Cmd_SetCommandCompletionFunc( "writeconfig", Cmd_CompleteWriteCfgName );
