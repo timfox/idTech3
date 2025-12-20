1cc26d2fb567bf8125fb50aa3205a6e3f004be0e
timfox <timfox@outlook.com>
2025-12-20 01:57:48 -0800

    Refactor include paths from `src/qcommon` to `src/common` in botlib and related files
    - Updated all relevant source files to reflect the new directory structure, ensuring consistency across the bot library.
    - Adjusted include statements in various files to point to the correct path, enhancing maintainability and clarity.

--- src/botlib/be_aas_bspq3.c
+++ src/botlib/be_aas_bspq3.c
@@ -29,8 +29,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  *****************************************************************************/
 
-#include "../qcommon/q_shared.h"
-#include "../qcommon/qcommon.h"
+#include "../common/q_shared.h"
+#include "../common/qcommon.h"
 #include "l_memory.h"
 #include "l_script.h"
 #include "l_precomp.h"
--- src/botlib/be_ai_chat.c
+++ src/botlib/be_ai_chat.c
@@ -33,8 +33,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  *****************************************************************************/
 
-#include "../qcommon/q_shared.h"
-#include "../qcommon/qcommon.h"
+#include "../common/q_shared.h"
+#include "../common/qcommon.h"
 #include "l_memory.h"
 #include "l_libvar.h"
 #include "l_script.h"
