e2d119a88d2d6833dc1774b95b2db9f44c1f1c93
timfox <timfox@outlook.com>
2025-12-30 18:20:18 -0800

    Add idTech8 Global Illumination view
    - Created a new PHP file for the idTech8 Global Illumination view, including title and description.
    - Added HTML structure with a header and descriptive paragraphs about the global illumination system, highlighting its dynamic capabilities for scene lighting.

---
+++ web/views/idtech8-global-illumination.php
@@ -0,0 +1,12 @@
+<?php
+
+$title = "idTech8 Global Illumination";
+$description = "idTech8 Global Illumination";
+
+?>
+
+<h1>idTech8 Global Illumination</h1>
+
+<p>idTech8 Global Illumination is a new global illumination system for idTech8. It is a fully dynamic global illumination system that can be used to light up any scene.</p>
+
+<p>It is a fully dynamic global illumination system that can be used to light up any scene.</p>
