a718fedb6cb090134d59c5ea08bf8a25a6ac2f19
timfox <timfox@outlook.com>
2026-01-25 02:53:13 -0800

    Add MP3 codec support to the sound system
    This commit introduces a new MP3 codec to the sound system, enabling playback of MP3 audio files. It includes the addition of relevant source files, header definitions, and integration into the existing sound codec framework. The CMake configuration is updated to allow enabling or disabling MP3 support through a new option. This enhancement improves audio playback capabilities and expands the range of supported audio formats in the project.

--- src/client/snd_codec.c
+++ src/client/snd_codec.c
@@ -130,6 +130,10 @@ void S_CodecInit( void )
 #ifdef USE_OGG_VORBIS
 	S_CodecRegister( &ogg_codec );
 #endif
+#ifdef USE_MP3
+	/* MP3 support */
+	S_CodecRegister( &mp3_codec );
+#endif
 
 	// Register wav codec last so that it is always tried first when a file extension was not found
 	S_CodecRegister( &wav_codec );
