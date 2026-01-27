0e2954061a914e7f5293cd3f0031f7883074e5ed
timfox <timfox@outlook.com>
2026-01-26 22:48:45 -0800

    Refactor: reorganize renderer structure and update include paths
    - Moved renderer-related files into dedicated directory for OpenGL and Vulkan.
    - Updated include paths in various source files to reflect the new structure.
    - Added new OpenGL and Vulkan renderer source files, including shaders and utility functions.
    - Enhanced CMakeLists.txt to accommodate the new source directories for better organization.

--- src/unix/linux_signals.c
+++ src/unix/linux_signals.c
@@ -31,7 +31,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "../qcommon/q_shared.h"
 #include "../qcommon/qcommon.h"
 #ifndef DEDICATED
-#include "../renderer/tr_local.h"
+#include "../renderers/openglrenderer/tr_local.h"
 #endif
 
 static qboolean signalcaught = qfalse;
