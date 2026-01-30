314dac2d326a864d3a8986280380c33b381a476e
timfox <timfox@outlook.com>
2026-01-30 08:43:02 -0800

    Limit ipv6 fallback to non-Linux/mac

--- src/qcommon/net_ip.c
+++ src/qcommon/net_ip.c
@@ -199,7 +199,7 @@ static SOCKET	ip6_socket = INVALID_SOCKET;
 static SOCKET	multicast6_socket = INVALID_SOCKET;
 
 // Ensure ipv6_mreq is defined for multicast operations on systems lacking it.
-#if !defined(_WIN32)
+#if !defined(_WIN32) && !defined(__linux__) && !defined(__APPLE__)
 #  ifndef IPV6_JOIN_GROUP
 struct ipv6_mreq {
 	struct in6_addr ipv6mr_multiaddr;
