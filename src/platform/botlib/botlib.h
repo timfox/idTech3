ceb819cbff49f98e4a88bd900d9f43d83aaf4669
timfox <timfox@outlook.com>
2026-02-01 12:57:12 -0800

    Refactor CMake configuration and remove unused headers
    - Simplified the CMake logic by removing conditional checks for USE_RENDERER_DLOPEN, ensuring consistent linking of libraries for OpenGL and Vulkan renderers.
    - Deleted unused header files for botlib, cgame, and Win32 platform, improving project organization and maintainability.
    - Introduced a new symbolic link for the external directory to streamline project structure.

--- src/platform/botlib/botlib.h
+++
@@ -1,6 +0,0 @@
-#ifndef PLATFORM_BOTLIB_BOTLIB_H
-#define PLATFORM_BOTLIB_BOTLIB_H
-
-#include "../../botlib/botlib.h"
-
-#endif // PLATFORM_BOTLIB_BOTLIB_H
--- src/platform/cgame/cg_public.h
+++
@@ -1,6 +0,0 @@
-#ifndef PLATFORM_CGAME_CG_PUBLIC_H
-#define PLATFORM_CGAME_CG_PUBLIC_H
-
-#include "../../cgame/cg_public.h"
-
-#endif // PLATFORM_CGAME_CG_PUBLIC_H
