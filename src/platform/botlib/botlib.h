af5f023b8c7f757c39006aff56249b7e738e3005
timfox <timfox@outlook.com>
2026-02-01 12:41:48 -0800

    Refactor CMake configuration and project structure for renderer libraries
    - Updated target link libraries for OpenGL and Vulkan renderers to conditionally include `qcommon` based on the USE_RENDERER_DLOPEN option, enhancing flexibility in linking.
    - Removed symbolic links for `botlib` and `cgame` directories and added corresponding header files to improve project organization and maintainability.

---
+++ src/platform/botlib/botlib.h
@@ -0,0 +1,6 @@
+#ifndef PLATFORM_BOTLIB_BOTLIB_H
+#define PLATFORM_BOTLIB_BOTLIB_H
+
+#include "../../botlib/botlib.h"
+
+#endif // PLATFORM_BOTLIB_BOTLIB_H
---
+++ src/platform/cgame/cg_public.h
@@ -0,0 +1,6 @@
+#ifndef PLATFORM_CGAME_CG_PUBLIC_H
+#define PLATFORM_CGAME_CG_PUBLIC_H
+
+#include "../../cgame/cg_public.h"
+
+#endif // PLATFORM_CGAME_CG_PUBLIC_H
