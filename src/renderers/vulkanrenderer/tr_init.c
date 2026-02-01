87591ae10fd4fd7af9b4133df0746c9482d86338
timfox <timfox@outlook.com>
2026-02-01 15:43:05 -0800

    Refactor conditional compilation for audio and renderer initialization
    - Updated `win_snd.c` to include conditional compilation for SDL usage, enhancing flexibility in sound initialization.
    - Modified `tr_init.c` to change the preprocessor directive for dynamic loading of the renderer, improving clarity in the modular renderer setup.
    - These changes aim to streamline the build process and improve the organization of platform-specific functionalities.

--- src/renderers/vulkanrenderer/tr_init.c
+++ src/renderers/vulkanrenderer/tr_init.c
@@ -281,7 +281,7 @@ static void R_ClearSymTables( void )
 
 
 // for modular renderer
-#ifndef USE_RENDERER_DLOPEN
+#ifdef USE_RENDERER_DLOPEN
 void QDECL Com_Error( errorParm_t code, const char *fmt, ... )
 {
 	char buf[ 4096 ];
