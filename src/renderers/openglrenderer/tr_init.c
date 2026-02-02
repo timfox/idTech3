f0d70654b3aaea2fcdfd6ac02aa34706f9d712cc
timfox <timfox@outlook.com>
2026-02-01 16:16:47 -0800

    Enhance project configuration for libpng and zlib integration
    - Added new property groups in `libpng.vcxproj` and `zlib.vcxproj` to define output and intermediate directories, improving build organization.
    - Updated `quake3e.vcxproj` to include references to `libpng` and `zlib`, ensuring proper linkage and enhancing project dependencies.
    - Modified `win_snd.c` to change the declaration of `dsound_init`, improving clarity in sound initialization.
    - Adjusted additional dependencies in `quake3e.vcxproj` to include `libpng` and `zlib` libraries, streamlining the build process.

--- src/renderers/openglrenderer/tr_init.c
+++ src/renderers/openglrenderer/tr_init.c
@@ -261,8 +261,7 @@ static void R_ClearSymTables( void )
 }
 
 
-// for modular renderer
-#ifndef USE_RENDERER_DLOPEN
+#if defined(USE_RENDERER_DLOPEN) && USE_RENDERER_DLOPEN
 void QDECL Com_Error( errorParm_t code, const char *fmt, ... )
 {
 	char buf[ 4096 ];
